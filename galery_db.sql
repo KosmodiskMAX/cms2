6504b69531a875f359ed865361e7d23a58c3aed7
KosmodiskMAX <37272936+KosmodiskMAX@users.noreply.github.com>
2018-09-16 11:42:20 +0200

    baza

--- galery_db.sql
+++ galery_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 15, 2018 at 09:09 PM
+-- Generation Time: Sep 16, 2018 at 11:41 AM
 -- Server version: 10.1.35-MariaDB
 -- PHP Version: 7.2.9
 
@@ -41,7 +41,8 @@ CREATE TABLE `comments` (
 
 INSERT INTO `comments` (`id`, `photo_id`, `user_id`, `body`) VALUES
 (7, 16, 1, 'Da li je ovo realno'),
-(9, 16, 1, 'Hoce kobas');
+(9, 16, 1, 'Hoce kobas'),
+(10, 16, 1, 'Idi nahui');
 
 -- --------------------------------------------------------
 
@@ -51,6 +52,7 @@ INSERT INTO `comments` (`id`, `photo_id`, `user_id`, `body`) VALUES
 
 CREATE TABLE `photos` (
   `id` int(3) NOT NULL,
+  `user_id` int(3) NOT NULL,
   `title` varchar(255) NOT NULL,
   `caption` varchar(255) NOT NULL,
   `description` text NOT NULL,
@@ -64,34 +66,35 @@ CREATE TABLE `photos` (
 -- Dumping data for table `photos`
 --
 
-INSERT INTO `photos` (`id`, `title`, `caption`, `description`, `filename`, `alternate_text`, `type`, `size`) VALUES
-(16, 'Batmobile', 'Brzina', '<p><i><strong>Mnogo brz auto, lorem ipsum hoce kobas max, prosto ne bi verovali</strong></i></p>', 'images-7.jpg', '', 'image/jpeg', 24140),
-(17, 'Auto3', '', '', '_large_image_4.jpg', '', 'image/jpeg', 554659),
-(18, 'Kobas2', '', '', '_large_image_2.jpg', '', 'image/jpeg', 309568),
-(19, 'Nova slika', '', '', 'images-4.jpg', '', 'image/jpeg', 23270),
-(20, 'Car 423', '', '', 'images-26.jpg', '', 'image/jpeg', 21802),
-(21, 'Javascript course 2', '', '', 'images-8.jpg', '', 'image/jpeg', 20810),
-(22, 'Car 4', '', '', 'images-14.jpg', '', 'image/jpeg', 21992),
-(23, 'Kobas', '', '', 'images-15.jpg', '', 'image/jpeg', 28466),
-(24, 'Kobas2', '', '', 'images-12.jpg', '', 'image/jpeg', 18540),
-(25, '1', '', '', 'images-50.jpg', '', 'image/jpeg', 21652),
-(26, '2', '', '', 'images-44.jpg', '', 'image/jpeg', 29486),
-(27, '3', '', '', 'images-43.jpg', '', 'image/jpeg', 27955),
-(28, '4', '', '', 'images-42.jpg', '', 'image/jpeg', 22401),
-(29, '5', '', '', 'images-41.jpg', '', 'image/jpeg', 16296),
-(30, '6', '', '', 'images-40.jpg', '', 'image/jpeg', 24385),
-(31, '7', '', '', 'images-39.jpg', '', 'image/jpeg', 24969),
-(32, '8', '', '', 'images-38.jpg', '', 'image/jpeg', 21857),
-(33, '9', '', '', 'images-37.jpg', '', 'image/jpeg', 20381),
-(34, '10', '', '', 'images-36.jpg', '', 'image/jpeg', 21672),
-(35, '20', '', '', 'images-35.jpg', '', 'image/jpeg', 23672),
-(36, '21', '', '', 'images-34.jpg', '', 'image/jpeg', 23587),
-(37, '22', '', '', 'images-33.jpg', '', 'image/jpeg', 16855),
-(38, '444', '', '', 'images-32.jpg', '', 'image/jpeg', 22772),
-(39, '123', '', '', 'images-31.jpg', '', 'image/jpeg', 20928),
-(40, '1234', '', '', 'images-29.jpg', '', 'image/jpeg', 25493),
-(41, '125', '', '', 'images-28.jpg', '', 'image/jpeg', 17662),
-(42, '126', '', '', 'images-18.jpg', '', 'image/jpeg', 27595);
+INSERT INTO `photos` (`id`, `user_id`, `title`, `caption`, `description`, `filename`, `alternate_text`, `type`, `size`) VALUES
+(16, 1, 'Batmobile', 'Brzina', '<p><i><strong>Mnogo brz auto, lorem ipsum hoce kobas max, prosto ne bi verovali</strong></i></p>', 'images-7.jpg', '', 'image/jpeg', 24140),
+(17, 1, 'Auto3', '', '', '_large_image_4.jpg', '', 'image/jpeg', 554659),
+(18, 1, 'Kobas2', '', '', '_large_image_2.jpg', '', 'image/jpeg', 309568),
+(19, 1, 'Nova slika', '', '', 'images-4.jpg', '', 'image/jpeg', 23270),
+(20, 1, 'Car 423', '', '', 'images-26.jpg', '', 'image/jpeg', 21802),
+(21, 1, 'Javascript course 2', '', '', 'images-8.jpg', '', 'image/jpeg', 20810),
+(22, 1, 'Car 4', '', '', 'images-14.jpg', '', 'image/jpeg', 21992),
+(23, 1, 'Kobas', '', '', 'images-15.jpg', '', 'image/jpeg', 28466),
+(24, 1, 'Kobas2', '', '', 'images-12.jpg', '', 'image/jpeg', 18540),
+(25, 1, '1', '', '', 'images-50.jpg', '', 'image/jpeg', 21652),
+(26, 1, '2', '', '', 'images-44.jpg', '', 'image/jpeg', 29486),
+(27, 1, '3', '', '', 'images-43.jpg', '', 'image/jpeg', 27955),
+(28, 1, '4', '', '', 'images-42.jpg', '', 'image/jpeg', 22401),
+(29, 1, '5', '', '', 'images-41.jpg', '', 'image/jpeg', 16296),
+(30, 1, '6', '', '', 'images-40.jpg', '', 'image/jpeg', 24385),
+(31, 1, '7', '', '', 'images-39.jpg', '', 'image/jpeg', 24969),
+(32, 1, '8', '', '', 'images-38.jpg', '', 'image/jpeg', 21857),
+(33, 1, '9', '', '', 'images-37.jpg', '', 'image/jpeg', 20381),
+(34, 1, '10', '', '', 'images-36.jpg', '', 'image/jpeg', 21672),
+(35, 1, '20', '', '', 'images-35.jpg', '', 'image/jpeg', 23672),
+(36, 1, '21', '', '', 'images-34.jpg', '', 'image/jpeg', 23587),
+(37, 1, '22', '', '', 'images-33.jpg', '', 'image/jpeg', 16855),
+(38, 1, '444', '', '', 'images-32.jpg', '', 'image/jpeg', 22772),
+(39, 1, '123', '', '', 'images-31.jpg', '', 'image/jpeg', 20928),
+(40, 1, '1234', '', '', 'images-29.jpg', '', 'image/jpeg', 25493),
+(41, 1, '125', '', '', 'images-28.jpg', '', 'image/jpeg', 17662),
+(42, 1, '126', '', '', 'images-18.jpg', '', 'image/jpeg', 27595),
+(43, 1, 'Dada', '', '', 'images-9.jpg', '', 'image/jpeg', 21108);
 
 -- --------------------------------------------------------
 
@@ -113,7 +116,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `firstname`, `lastname`, `user_image`) VALUES
-(1, 'Superman', '123', 'Mihajlo', 'Petrovic', '_large_image_3.jpg'),
+(1, 'Superman', '123', 'Mihajlo', 'Petrovic', 'luna_djogani_decko_show_clip_1000x0.jpg'),
 (4, 'KONJ', '', '', '', ''),
 (10, 'KONJ33', '123', 'Lazar', 'Casio', ''),
 (11, 'KONJ', '123', '', '', ''),
@@ -152,13 +155,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `photos`
 --
 ALTER TABLE `photos`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `users`
